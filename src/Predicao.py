5e543d4f1a04c6b15d027706d84e60473d4e019b
Ives Yoshida <yoshidaives@gmail.com>
2024-11-21 16:20:56 -0300

    Ajustes finais

--- src/Predicao.py
+++ src/Predicao.py
@@ -13,8 +13,12 @@ class PredicaoAlzheimer:
         3:'Alzheimer Moderada'
         }
 
-    def PredicaoImagem(self, imagem):
-        predicao = self.model.predict(imagem)
+    def __init__(self, imagem):
+        self.imagem = imagem
+        self.predicao = self.PredicaoImagem()
+
+    def PredicaoImagem(self):
+        predicao = self.model.predict(self.imagem)
         predicaoArgmax = np.argmax(predicao, axis=1)
         predicaoClasse = self.classes.get(predicaoArgmax[0], 1)
         return predicaoClasse
@@ -28,9 +32,14 @@ class PredicaoEM:
         0:'Sem EM',
         1:'EM'
         }
+    
+    def __init__(self, imagem):
+        self.imagem = imagem
+        self.predicao = self.PredicaoImagem()
+    
 
-    def PredicaoImagem(self, imagem):
-        predicao = self.model.predict(imagem)
+    def PredicaoImagem(self):
+        predicao = self.model.predict(self.imagem)
         predicaoArgmax = np.argmax(predicao, axis=1)
         predicaoClasse = self.classes.get(predicaoArgmax[0], 1)
         return predicaoClasse
--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from Predicao import PredicaoAlzheimer, PredicaoEM
 def main():
     image = Imagem.LeituraImagem()
     if image is not None:
-        resultadoAlzheimer = PredicaoAlzheimer.PredicaoImagem(image)
-        resultadoEM = PredicaoEM.PredicaoImagem(image)
-        print(resultadoAlzheimer, resultadoEM)
+        alzheimer = PredicaoAlzheimer(image)
+        EM = PredicaoEM(image)
+        resultadoAlzheimer = alzheimer.predicao
+        resultadoEM = EM.predicao
+        print(f'{resultadoAlzheimer}, {resultadoEM}')
 
 if __name__ == '__main__':
     main()
